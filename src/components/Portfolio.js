d3563ba3100705db9fc4022e580b5ce6d47a97ea
Ranga <rangachimalgi123@gmail.com>
2024-08-31 10:40:24 +0530

    changes in experience

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,71 +12,199 @@ const Portfolio = () => {
     <div className="experience-container">
       <FontAwesomeIcon icon={faBriefcase} />
       <h2>Experience</h2>
-      <div className="experience-item">
-        <FontAwesomeIcon icon={faBuilding} className="company-logo" />
-        <p>
-          <span className="company-name">
-            Company (CustomerXPs) - Software Engineer (Trainee)
-          </span>{" "}
-          <br />
-          <span className="duration">Duration: 2 months</span> <br />
-          - Received comprehensive training on the company's product stack,
-          including technologies such as React.js, Java, and GitHub. <br />
-          - Developed a full-stack website for KYC validation using React.js,
-          Java, and MySQL during the training period. <br />- Gained hands-on
-          experience in building responsive user interfaces and backend
-          functionalities.
-        </p>
-      </div>
-      <div className="experience-item">
-        <p>
-          <span className="company-name">
-            Company (CustomerXPs) - Software Engineer
-          </span>{" "}
-          <br />
-          <span className="duration">Duration: 3 months</span> <br />
-          - Worked on Java-based product development, resolving critical issues,
-          and enhancing product features. <br />- Collaborated with the team to
-          deliver high-quality solutions and ensured the product's stability and
-          performance.
-        </p>
-      </div>
-      <div className="experience-item">
-        <p>
-          <span className="company-name">
-            Company (CustomerXPs) - Software Engineer
-          </span>{" "}
-          <br />
-          <span className="duration">Duration: 4 months</span> <br />
-          - Transitioned to a new team and project, primarily focusing on
-          React.js frontend development. <br />
-          - Led the design and implementation of the user interface for a new
-          project within the product. <br />- Collaborated with cross-functional
-          teams to ensure seamless integration and user experience.
-        </p>
-      </div>
-      <div className="experience-item">
-        <FontAwesomeIcon icon={faUserTie} className="freelancing-logo" />
 
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">Fortune Blood Test Platform</span>
+          <span className="duration">Freelancing</span>
+        </div>
         <p>
-          <span className="company-name">Freelance Software Engineer</span>{" "}
+          <a href="https://fortunebloodtest.com/">Live Website</a>
           <br />
-          <span className="duration">Project 1: Ongoing</span> <br />- Developed
-          a web application using MERN for a client who owns an Engineering shop
-          for electricals. <br />
-          - Collaborated closely with the client to understand requirements and
-          implement custom features. <br />- Currently working on enhancements
-          and updates for the same project. <br />
+          Spearheaded the development of a platform enabling users to book
+          various blood tests and health packages with ease. The project
+          emphasized a seamless user experience, incorporating a powerful admin
+          panel for order management. This included features for securely
+          uploading and emailing reports to users. Currently integrating a
+          payment gateway to streamline financial transactions.
+          <br />
+          <strong>Technologies:</strong> MongoDB, Express.js, React, Node.js
+          (MERN Stack)
         </p>
       </div>
+      <div className="divider"></div>
+
       <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">SRS Matha Database Application</span>
+          <span className="duration">Freelancing</span>
+        </div>
         <p>
-          <span className="company-name">Freelance Software Engineer</span>{" "}
+          <a href="https://github.com/rangachimalgi/matha-sr">
+            GitHub Repository
+          </a>
           <br />
-          <span className="duration">Project 2: In Progress</span> <br />
-          - Building a web application for a client who owns an electronic shop
-          using MERN. <br />- Responsible for frontend development and
-          integrating backend APIs. <br />
+          Created an internal database application for temple management,
+          focusing on ease of use for administrative tasks. The system features
+          a flexible admin panel designed to handle data entry across various
+          fields, with the scalability to accommodate future enhancements. The
+          application has been reported to significantly improve internal data
+          management processes.
+          <br />
+          <strong>Technologies:</strong> MongoDB, Express.js, React, Node.js
+          (MERN Stack)
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">Career Net HR Portal</span>
+          <span className="duration">Freelancing</span>
+        </div>
+        <p>
+          <a href="https://careermap.net.in/">Live Website</a>
+          <br />
+          Developed a human resources portal aimed at streamlining the
+          recruitment process across various industries. The portal focuses on
+          delivering a user-friendly experience for both recruiters and job
+          seekers, facilitating smoother workflows and improved hiring outcomes.
+          The fully operational site has been well-received by the client for
+          its performance and ease of use.
+          <br />
+          <strong>Technologies:</strong> React, with additional tools and
+          libraries
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">SRS Desktop Billing Software</span>
+          <span className="duration">Freelancing</span>
+        </div>
+        <p>
+          <a href="https://github.com/rangachimalgi/srs-desktop">
+            GitHub Repository
+          </a>
+          <br />
+          Developed an internal billing application for temple management,
+          utilizing MongoDB, React, and Electron. The software is designed for
+          efficiency and ease of use by administrative staff, managing billing
+          operations effectively. Ongoing enhancements are being made based on
+          client feedback to continually improve the user interface and
+          functionality.
+          <br />
+          <strong>Technologies:</strong> MongoDB, React, Electron
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">
+            Dratha Creations Event Management Platform
+          </span>
+          <span className="duration">Freelancing</span>
+        </div>
+        <p>
+          <a href="https://dratha-creations-t4ls.onrender.com">Live Website</a>
+          <br />
+          Led the development of an event management platform with an emphasis
+          on category management through an intuitive admin panel. The panel
+          allows administrators to easily manage categories and subcategories,
+          with these changes reflected dynamically in the frontend. The project
+          includes advanced user and order management features, and plans are in
+          place to integrate a payment gateway for future transactions.
+          <br />
+          <strong>Technologies:</strong> MongoDB, Express.js, React, Node.js
+          (MERN Stack)
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">Shree Renuka E-Commerce Platform</span>
+          <span className="duration">Freelancing</span>
+        </div>
+        <p>
+          <a href="https://github.com/rangachimalgi/shree-renuka/">
+            GitHub Repository
+          </a>
+          <br />
+          Developed a robust e-commerce platform tailored for electrical
+          products, offering an intuitive shopping experience for items such as
+          cables, wires, and gearboxes. The project involved designing and
+          implementing an advanced admin panel, allowing efficient management of
+          products, including adding, updating, and removing items. This
+          platform significantly improved business operations and led to
+          multiple client orders.
+          <br />
+          <strong>Technologies:</strong> MongoDB, Express.js, React, Node.js
+          (MERN Stack)
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">Existing Projects</span>
+          <span className="duration">Freelancing</span>
+        </div>
+        <p>
+          Worked on several existing projects on lavarel project and MERN
+          <br />
+          Code Fixes
+          <br />
+          UI changes in an existing project using React Js
+          <br />
+          <strong>Technologies:</strong> Php lavarel, React, Node.js
+        </p>
+      </div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">
+            Customer XPS - Full Stack Developer
+          </span>
+          <span className="duration">May 2022 - March 2023</span>
+        </div>
+        <p>
+          <strong>Web Application Development:</strong> Played a key role in the
+          development and maintenance of a complex web application, focusing on
+          resolving various technical issues to enhance functionality and
+          performance. Utilized Java for backend development, with React.js,
+          Redux, and JavaScript on the frontend, alongside managing MySQL
+          database operations.
+          <br />
+          <br />
+          <strong>User Interface Design for New Project:</strong> Led the
+          creation of a dynamic and responsive user interface for a new project,
+          starting from scratch. Responsible for the architecture, design, and
+          implementation of the frontend using React.js, JavaScript, and Redux.
+          Worked on integrating frontend components with the backend, resolving
+          issues related to API endpoints and database interactions.
+          <br />
+          <strong>Technologies:</strong> Java, React.js, Redux, JavaScript,
+          MySQL
+        </p>
+      </div>
+      <div className="divider"></div>
+
+      <div className="experience-item">
+        <div className="experience-header">
+          <span className="company-name">Customer XPS - Training</span>
+          <span className="duration">May 2022 - March 2023</span>
+        </div>
+        <p>
+          <strong>KYC Website Project:</strong> During my training period at
+          Customer XPS, I participated in the development of a Know Your
+          Customer (KYC) project through pair programming. The project, built
+          using React.js, Redux, Java, and MySQL, received approval from both
+          the project manager and the CTO. This experience provided me with deep
+          insights into full-stack development and best practices in software
+          engineering.
         </p>
       </div>
     </div>
